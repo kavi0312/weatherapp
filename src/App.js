9faa7abd69d76071ba7c0fecf9f43354036435be
kavi0393 <singhkavitahts@gmail.com>
2022-03-13 19:52:59 +0530

    commiting weather app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Weatherapp from "./Components/Weather/Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <Weatherapp />
+    </div>
+  );
+}
+
+export default App;
